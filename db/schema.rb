1a26823473fc2744aad0a47e7c685f71d0707c8a
xavier poh <xvrpke@gmail.com>
2017-03-02 16:07:57 +0800

    merged articles, comments and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170302073943) do
 
   create_table "articles", force: :cascade do |t|
