9c53a667bb6f157771bb156a481bfd720ec69350
Justin Teo <justin.tsw@gmail.com>
2017-03-02 10:57:21 +0800

    Added About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
   get 'welcome/index'   #get request going to url welcome/index
 
   root 'welcome#index'     #Setting homepage e.g localhost:3000 to render welcome/index
