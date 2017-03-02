66cfe04ea35d966d9a5d84b6229c28b70d09c305
xavier poh <xvrpke@gmail.com>
2017-03-02 14:15:55 +0800

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'comments/index'
+
   get 'contact/index'
   get 'faq/index'
   get 'about/index'
   get 'welcome/index'   #get request going to url welcome/index
 
+  resources :comments
   resources :articles
   root 'welcome#index'     #Setting homepage e.g localhost:3000 to render welcome/index
 
